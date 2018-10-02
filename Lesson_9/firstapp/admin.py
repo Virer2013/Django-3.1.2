8ced9ea378b4b21912ee4666f9049eec7e45de12
konstantin <Virer2007@mail.ru>
2018-10-02 10:36:41 +0300

    Add Lesson 9

---
+++ Lesson_9/firstapp/admin.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+from firstapp.models import PizzaShop
+from firstapp.models import Pizza
+
+# Register your models here.
+admin.site.register(PizzaShop)
+admin.site.register(Pizza)
