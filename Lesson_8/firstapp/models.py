aeb71edbb71b8d961811828b0b7163a556095ba8
konstantin <Virer2007@mail.ru>
2018-10-02 10:23:12 +0300

    Add Lesson 8

---
+++ Lesson_8/firstapp/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+# Create your models here.
+class PizzaShop(models.Model):
+    name = models.CharField(max_length=30, verbose_name='Пиццерия')
+    description = models.TextField(verbose_name='Описание')
+    rating = models.FloatField(default=0, verbose_name='Рейтинг')
+    url = models.URLField(verbose_name='Интернет-адрес пиццерии')
+
+    def __str__(self):
+        return self.name
