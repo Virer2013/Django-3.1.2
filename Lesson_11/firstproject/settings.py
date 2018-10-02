a3474b153c6a4cd7121cc2e14aba97f3dffe43ec
konstantin <Virer2007@mail.ru>
2018-10-02 11:28:23 +0300

    Fix

--- Lesson_11/firstproject/settings.py
+++ Lesson_11/firstproject/settings.py
@@ -38,8 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'firstapp',
-    'testurlapp',
-    'teststaticapp'
+    'testurlapp'
 ]
 
 MIDDLEWARE = [
--- Lesson_12/firstproject/urls.py
+++ Lesson_12/firstproject/urls.py
@@ -16,9 +16,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 # from testurlapp import views
+from teststaticapp import views
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    # path('', views.home, name='home'),
-    path('test_app/', include('testurlapp.test_urls'))
+    path('', views.home, name='index'),
+    # path('test_app/', include('testurlapp.test_urls'))
 ]
