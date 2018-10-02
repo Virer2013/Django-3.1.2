ec758ece81a9d060a6669dc8389128d6b21477fe
konstantin <Virer2007@mail.ru>
2018-10-02 11:06:21 +0300

    Add Lesson 10

---
+++ Lesson_10/firstapp/models.py
@@ -0,0 +1,29 @@
+from django.db import models
+
+# Create your models here.
+class PizzaShop(models.Model):
+    name = models.CharField(max_length=30, verbose_name='Пиццерия')
+    description = models.TextField(verbose_name='Описание')
+    rating = models.FloatField(default=0, verbose_name='Рейтинг')
+    url = models.URLField(verbose_name='Интернет-адрес пиццерии')
+
+    class Meta:
+        verbose_name='Пиццерия'
+        verbose_name_plural='Пиццерии'
+
+    def __str__(self):
+        return self.name
+
+class Pizza(models.Model):
+    pizzashop = models.ForeignKey(PizzaShop, on_delete=models.CASCADE)
+    name = models.CharField(max_length=30, verbose_name='Название пиццы')
+    short_description = models.CharField(max_length=30, verbose_name='Краткое описание')
+    price = models.IntegerField(default=0, verbose_name='Цена')
+
+    class Meta:
+        verbose_name='Пицца'
+        verbose_name_plural='Пиццы'
+        ordering=['name']
+
+    def __str__(self):
+        return self.name
---
+++ Lesson_10/testurlapp/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class TesturlappConfig(AppConfig):
+    name = 'testurlapp'
