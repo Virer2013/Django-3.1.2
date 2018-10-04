9f9a16306862edae073bb8e16375a15ffab73d40
konstantin <Virer2007@mail.ru>
2018-10-04 13:24:09 +0300

    Add Lesson 45

---
+++ Lesson_45/pizzashopapp/apis.py
@@ -0,0 +1,22 @@
+from django.http import JsonResponse
+
+from .models import PizzaShop, Pizza
+from pizzashopapp.serializers import PizzaShopSerializer, PizzaSerializer
+
+def client_get_pizzashops(request):
+    pizzashops = PizzaShopSerializer(
+        PizzaShop.objects.all().order_by('-id'),
+        many=True,
+        context={'request':request}
+    ).data
+
+    return JsonResponse({'pizzashops': pizzashops})
+
+def client_get_pizzas(request, pizzashop_id):
+    pizzas= PizzaSerializer(
+        Pizza.objects.all().filter(pizzashop_id=pizzashop_id).order_by('-id'),
+        many=True,
+        context={'request':request}
+    ).data
+
+    return JsonResponse({'pizzas': pizzas})
